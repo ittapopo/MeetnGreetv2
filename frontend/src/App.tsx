efce3b60a9476c38e991ad52b765466b89546258
Ittapopo <stianstian95gmail.com>
2021-10-01 14:11:35 +0200

    Getting emotion to work

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+import { fontFamily, fontSize, gray2 } from './Styles';
+
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+
+function App() {
+  return (
+    <div
+    css={css`
+    font-family: ${fontFamily};
+    font-size: ${fontSize};
+    color: ${gray2};
+  `}
+    >
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
