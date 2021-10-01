a83526c9e12ff329a01be4d16ea45b34c904f2f7
Ittapopo <stianstian95gmail.com>
2021-10-01 15:12:49 +0200

    Fixed more styling problems

--- frontend/src/Icons.tsx
+++ frontend/src/Icons.tsx
@@ -1,5 +1,17 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react';
+
 import React from 'react';
 import user from './user.svg';
 
-export const UserIcon = () =>
-    <img src={user} alt="User" width="12px" />;
+export const UserIcon = () => (
+    <img
+      src={user}
+      alt="User"
+      css={css`
+        width: 16px;
+        opacity: 0.6;
+      `}
+    />
+  );
