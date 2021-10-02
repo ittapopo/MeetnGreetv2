08af2f047364e928d19769240443494736d888b2
Ittapopo <stianstian95gmail.com>
2021-10-02 14:15:40 +0200

    implementing props and more styling

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import { PrimaryButton } from './Styles';
 
 export const HomePage = () => (
-    <div>
-        <div>
-            <h2>Unanswered Meeting</h2>
-            <button>Join meeting</button>
+    <div
+        css={css`
+            margin: 50px auto 20px auto;
+            padding: 30px 20px;
+            max-width: 600px;
+        `}>
+        <div
+            css={css`
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+            `}>
+            <h2
+                css={css`
+                    font-size: 15px;
+                    font-weight: bold;
+                    margin: 10px 0px 5px;
+                    text-align: center;
+                    text-transform: uppercase;
+                `}
+            >Unanswered Meeting</h2>
+            <PrimaryButton>Join meeting</PrimaryButton>
         </div>
     </div>
 )
