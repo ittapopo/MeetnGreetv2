5422262283ed6a725c236de9e30ebdc2546b90c4
Ittapopo <stianstian95gmail.com>
2021-10-04 13:27:46 +0200

    made meetingList async and added loading

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -3,11 +3,25 @@
 import { css, jsx } from '@emotion/react';
 import { PrimaryButton } from './Styles';
 import { MeetingList } from './MeetingList';
-import { getNewMeetings } from './MeetingsData';
+import { getNewMeetings, MeetingData } from './MeetingsData';
 import { Page } from './Page';
 import { PageTitle } from './PageTitle';
+import { useEffect, useState } from 'react';
 
-export const HomePage = () => (
+export const HomePage = () => {
+    const [meetings, setMeetings] = useState<MeetingData[] | null>(null);
+    const [meetingsLoading, setMeetingsLoading] = useState(true);
+
+    useEffect(() => {
+        const doGetNewMeetings = async () => {
+            const newMeetings = await getNewMeetings();
+            setMeetings(newMeetings);
+            setMeetingsLoading(false);
+        };
+        doGetNewMeetings();
+    }, []);
+
+   return ( 
     <Page>
         <div
             css={css`
@@ -18,6 +32,18 @@ export const HomePage = () => (
             <PageTitle>Unanswered Meeting</PageTitle>
             <PrimaryButton>Join meeting</PrimaryButton>
         </div>
-        {/* <MeetingList data={getNewMeetings()} />} */}
+        {meetingsLoading ? (
+            <div
+                css={css`
+                    font-size: 16px;
+                    font-style: italic;
+                `}
+            >
+                Loading...
+            </div>
+        ) : (
+        <MeetingList data={meetings || []} />
+        )}
     </Page>
-);
+  );
+};
