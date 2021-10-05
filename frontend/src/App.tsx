f41cac8739922ba5f07174e26b4491f5ad69f917
Ittapopo <stianstian95gmail.com>
2021-10-05 13:42:36 +0200

    fixed routing issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 import { fontFamily, fontSize, gray2 } from './Styles';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import { AskPage } from './AskPage';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
+import { NotFoundPage } from './NotFoundPage';
 
 /** @jsxRuntime classic */
 /** @jsx jsx */
@@ -20,11 +21,15 @@ function App() {
     font-size: ${fontSize};
     color: ${gray2};
   `}>
-      <Header />
+    <Header />
+    <Switch>
+      <Redirect from="/home" to="/" />
       <Route exact path="/" component={HomePage} />
       <Route path="/search" component={SearchPage} />
       <Route path="/ask" component={AskPage} />
       <Route path="/signin" component={SignInPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
     </div>
     </BrowserRouter>
   );
